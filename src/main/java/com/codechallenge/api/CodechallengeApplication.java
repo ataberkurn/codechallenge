b2d6c10948a1b4758dd485b3b3b9fd422a495f95
ataberk <ataberkurn59@gmail.com>
2024-10-01 23:20:45 +0300

    - init commit

---
+++ src/main/java/com/codechallenge/api/CodechallengeApplication.java
@@ -0,0 +1,13 @@
+package com.codechallenge.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodechallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodechallengeApplication.class, args);
+    }
+
+}
